0f271a55d2efe518829d4246c36ed4716df8117a
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-08-23 18:58:43 +0900

    Install codedeploy-agent

--- packer/scripts/ubuntu/install.sh
+++ packer/scripts/ubuntu/install.sh
@@ -5,6 +5,8 @@ apt-get update && apt-get upgrade -y
 apt-get install -y \
   curl \
   git \
+  python-pip \
+  ruby2.0 \
   wget \
   && :
 
@@ -16,3 +18,11 @@ curl https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz \
   | tar -C /usr/local -xzf -
 
 /usr/local/go/bin/go version
+
+pip install awscli
+aws --version
+
+curl -L http://aws-codedeploy-ap-northeast-1.s3.amazonaws.com/latest/install \
+  | sed -e 's/gdebi/gdebi-core/g' > /tmp/install
+chmod +x /tmp/install
+/tmp/install auto
